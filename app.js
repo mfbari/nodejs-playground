79975eb0d686d27154cd5d2319f31e95fbab4275
Faizul Bari <faizulbari@gmail.com>
2023-04-14 23:50:14 +0600

    Add body parser

--- app.js
+++ app.js
@@ -1,17 +1,39 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const app = express()
 app.disable('x-powered-by')
 
-app.use((request, response, next) => {
-  console.log("In first middleware")
-  next()
+app.use(bodyParser.urlencoded({
+  extended: false
+}))
+
+app.use("/add-product", (request, response, next) => {
+  console.log("In the /add-product middleware")
+  response.send("<form action='/product' method='POST'><input type='text' name='title'><button type='submit'>Add Product</button></form>")
 })
 
-app.use((request, response, next) => {
-  console.log("In second middleware")
-  response.send("<h1>Hello World!</h1>")
+app.use("/product", (request, response, next) => {
+  console.log("In the /product middleware")
+  console.log("request body:", request.body)
+  console.log("request body title:", request.body.title)
+  response.send("<h1>The 'Product' Page</h1>")
 })
+
+app.use("/", (request, response, next) => {
+  console.log("In the / middleware")
+  response.send("<h1>The 'Home' Page</h1>")
+})
+
+// app.use((request, response, next) => {
+//   console.log("In first middleware")
+//   next()
+// })
+
+// app.use((request, response, next) => {
+//   console.log("In second middleware")
+//   response.send("<h1>Hello from Express!</h1>")
+// })
 
 app.listen(7999, () => {
   console.log('Server is running on port 7999')

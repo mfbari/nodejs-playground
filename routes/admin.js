eb42ef566bad3c643025fc20e3a499681ff75aab
Faizul Bari <faizulbari@gmail.com>
2023-04-15 15:24:41 +0600

    Link the routes to return html pages

--- routes/admin.js
+++ routes/admin.js
@@ -1,16 +1,18 @@
+const path = require('path')
 const express = require('express')
 const router = express.Router()
 
 router.get("/add-product", (request, response, next) => {
   console.log("In the /add-product middleware")
-  response.send("<form action='/admin/add-product' method='POST'><input type='text' name='title'><button type='submit'>Add Product</button></form>")
+  response.sendFile(path.join(__dirname, "../", "views", "add-product.html"))
 })
 
 router.post("/add-product", (request, response, next) => {
   console.log("In the /product middleware")
   console.log("request body:", request.body)
   console.log("request body title:", request.body.title)
-  response.send("<h1>The 'Product' Page</h1>")
+  response.sendFile(path.join(__dirname, "../", "views", "add-product.html"))
+  // response.send("<h1>The 'Product' Page</h1>")
 })
 
 module.exports = router
--- routes/shop.js
+++ routes/shop.js
@@ -1,9 +1,10 @@
+const path = require('path')
 const express = require('express')
 const router = express.Router()
 
 router.get("/", (request, response, next) => {
   console.log("In the / middleware")
-  response.send("<h1>The 'Home' Page</h1>")
+  response.sendFile(path.join(__dirname, "../", "views", "shop.html"))
 })
 
 module.exports = router

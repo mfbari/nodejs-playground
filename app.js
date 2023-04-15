e62516233965b1cf26a4fcaca79a23d2b44f6b2c
Faizul Bari <faizulbari@gmail.com>
2023-04-15 14:55:25 +0600

    Remove commented out code

--- app.js
+++ app.js
@@ -18,16 +18,6 @@ app.use((request, response, next) => {
   response.status(404).send("<h1>Page not found</h1>")
 })
 
-// app.use((request, response, next) => {
-//   console.log("In first middleware")
-//   next()
-// })
-
-// app.use((request, response, next) => {
-//   console.log("In second middleware")
-//   response.send("<h1>Hello from Express!</h1>")
-// })
-
 app.listen(7999, () => {
   console.log('Server is running on port 7999')
 })
